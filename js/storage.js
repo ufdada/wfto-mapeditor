b3e67e7194f01f56a18e2f3ac4b4bac6b0842996
ufdada <bloody_impact@web.de>
2015-03-17 21:38:52 +0100

    a little beautify

--- js/storage.js
+++ js/storage.js
@@ -33,14 +33,14 @@ function dataStorage() {
 			returnValue = localStorage.getItem(name);
 		}
 		var nameEQ = name + "=";
-		var ca = document.cookie.split(';');
-		for(var i=0;i < ca.length;i++) {
-			var c = ca[i];
-			while (c.charAt(0)==' ') {
-				c = c.substring(1,c.length);
+		var cookies = document.cookie.split(';');
+		for(var i=0;i < cookies.length;i++) {
+			var cookie = cookies[i];
+			while (cookie.charAt(0)==' ') {
+				cookie = cookie.substring(1, cookie.length);
 			}
-			if (c.indexOf(nameEQ) === 0) {
-				returnValue = c.substring(nameEQ.length, c.length);
+			if (cookie.indexOf(nameEQ) === 0) {
+				returnValue = cookie.substring(nameEQ.length, cookie.length);
 			}
 		}
 		
