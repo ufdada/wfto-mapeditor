74b953748c31bad311e8a6fd6b12894a986db436
ufdada <bloody_impact@web.de>
2015-03-18 23:59:18 +0100

    cache test map (reduces overall test time)

---
+++ tests/maps/cacheMap.js
@@ -0,0 +1,19 @@
+module.exports = {
+	'save': function (test, external) {
+		test.execute(function() {
+			this.data("mapData", window.terrain.exportData());
+		});
+		return test;
+	},
+	'load': function (test, external) {
+		test.execute(function() {
+			// reset these checkoxes, since dalek doesn't do that (wether with a second click nor through a reload) 
+			window.extend.checked = "";
+			window.reverse.checked = "";
+			window.rotate.checked = "";
+			// reimport the map
+			window.terrain.importData(this.data("mapData"));
+		});
+		return test;
+	}
+};
