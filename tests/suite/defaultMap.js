81e6212030b34a2fcdf40d8c36c29165da8ff229
ufdada <bloody_impact@web.de>
2015-03-18 17:06:21 +0100

    add new mirror option test to suite

--- tests/suite/defaultMap.js
+++ tests/suite/defaultMap.js
@@ -5,14 +5,25 @@ var new_map = require('../options/new_map');
 module.exports = {
     "Mirroring": function(test) {
 		test.open("index.html");
-		new_map.generate(test, true);
-		map.drawMap(test, true);
-		mirror1.mirror(test, true);
-		test.execute(function() {
-			this.assert.ok(window.terrain.mapsizex == 20, "window.terrain.mapsizex == 20");
-			this.assert.ok(window.terrain.mapsizey == 20, "window.terrain.mapsizey == 20");
-			this.assert.ok(Object.keys(window.terrain.tiles).length == 8, "Object.keys(window.terrain.tiles).length == " + Object.keys(window.terrain.tiles).length);
-		});
+			// Mirror 1
+			new_map.generate(test, true);
+			map.drawMap(test, true);
+			mirror1.mirror(test, true);
+			test.execute(function() {
+				this.assert.ok(window.terrain.mapsizex == 20, "window.terrain.mapsizex == 20");
+				this.assert.ok(window.terrain.mapsizey == 20, "window.terrain.mapsizey == 20");
+				this.assert.ok(Object.keys(window.terrain.tiles).length == 8, "Object.keys(window.terrain.tiles).length == 8");
+			});
+			// Mirror 1 Extend
+			new_map.generate(test, true);
+			map.drawMap(test, true);
+			mirror1.mirrorExtend(test, true);
+			test.execute(function() {
+				this.assert.ok(window.terrain.mapsizex == 40, "window.terrain.mapsizex == 40");
+				this.assert.ok(window.terrain.mapsizey == 40, "window.terrain.mapsizey == 40");
+				this.assert.ok(Object.keys(window.terrain.tiles).length == 20, "Object.keys(window.terrain.tiles).length == 20");
+			});
         test.done();
-    }
+    }	
+			
 };
