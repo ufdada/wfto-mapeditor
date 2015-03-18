22d994c3ccdabf2b17cdce7dc70b9e85017be1b6
ufdada <bloody_impact@web.de>
2015-03-18 17:05:23 +0100

    add extended to mirror 1 test

--- tests/options/mirror1.js
+++ tests/options/mirror1.js
@@ -3,12 +3,36 @@ module.exports = {
 		test.click('#optionButton')
 			.click('#first')
 			.assert.enabled('#rotate', 'Rotate enabled')
+			.assert.notSelected('#rotate', 'Rotate unchecked')
 			.assert.disabled('#reverse', 'Reverse disabled')
 			.assert.enabled('#extend', 'Extend enabled')
+			.assert.notSelected('#extend', 'Extend unchecked')
 			.click('#mirrorButton')
-			//.waitFor(function () { return document.getElementById("options").style.display == none;}, [], 500) //('#options')
 			.screenshot("./tests/images/:browser/mirror1.png");
 		
+		if (external) {
+			return test;
+		} else {
+			test.done();
+		}
+	},
+	'mirrorExtend': function (test, external) {
+
+		if (!external) { test.open('index.html'); }
+
+		test
+			.click('#optionButton')
+			.click('#first')
+			.click('#extend')
+			.assert.enabled('#rotate', 'Rotate enabled')
+			.assert.notSelected('#rotate', 'Rotate unchecked')
+			.assert.disabled('#reverse', 'Reverse disabled')
+			.assert.enabled('#extend', 'Extend enabled')
+			.assert.selected('#extend', 'Extend unchecked')
+			.click('#mirrorButton')
+			.click('#extend')
+			.screenshot("./tests/images/:browser/mirror1Extend.png");
+
 		if (external) {
 			return test;
 		} else {
