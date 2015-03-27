4c3a7b50008b9253dda9e8b324791bf26ef52660
ufdada <bloody_impact@web.de>
2015-03-27 14:17:16 +0100

    add missing semicolon

--- tests/options/undo_redo.js
+++ tests/options/undo_redo.js
@@ -8,15 +8,15 @@ module.exports = {
 				this.data("redoHistory", window.terrain.redoHistory);
 			})
 			.assert.enabled("#undo", "Undo should be enabled!")
-			
+
 			.click('#undo')
-			
+
 			.execute(function(){
 				this.assert.ok(window.terrain.undoHistory.length == this.data("undoHistory").length - 1, "undoHistory.length should be " + (this.data("undoHistory").length - 1) + ", was " + window.terrain.undoHistory.length);
 				this.assert.ok(window.terrain.redoHistory.length == this.data("redoHistory").length + 1, "redoHistory.length should be " + (this.data("redoHistory").length + 1) + ", was " + window.terrain.redoHistory.length);
 			})
 			.assert.enabled("#redo", "Redo should be enabled!")
-			.assert.enabled("#undo", "Undo should be enabled!")
+			.assert.enabled("#undo", "Undo should be enabled!");
 
 		makeShot && test.screenshot("./tests/images/:browser/undo.png");
 
@@ -35,14 +35,14 @@ module.exports = {
 				this.data("redoHistory", window.terrain.redoHistory);
 			})
 			.assert.enabled("#redo", "Redo should be enabled!")
-			
+
 			.click('#redo')
-			
+
 			.execute(function(){
 				this.assert.ok(window.terrain.undoHistory.length == this.data("undoHistory").length + 1, "undoHistory.length should be " + (this.data("undoHistory").length + 1) + ", was " + window.terrain.undoHistory.length);
 				this.assert.ok(window.terrain.redoHistory.length == this.data("redoHistory").length - 1, "redoHistory.length should be " + (this.data("redoHistory").length - 1) + ", was " + window.terrain.redoHistory.length);
 				this.data("mapData", window.terrain.exportData());
-			})
+			});
 
 		makeShot && test.screenshot("./tests/images/:browser/redo.png");
 
