170b9bbea4c1629905eb9b955c20da2b7c950d1a
ufdada <bloody_impact@web.de>
2015-03-27 13:07:56 +0100

    write tests for redo_undo feature

--- tests/suite/options.js
+++ tests/suite/options.js
@@ -1,6 +1,7 @@
 var cache = require('../maps/cacheMap');
 var exchange = require('../options/exchange_map');
 var resize_map = require('../options/resize_map');
+var undo_redo = require('../options/undo_redo');
 
 var helper = require('../helper');
 // Make screenshots
@@ -67,15 +68,78 @@ module.exports = {
 
 		test.done();
 	},
-	"Undomanager": function(test) {
+	"Undomanager part 1": function(test) {
 		console.log(">> Test Undomanager".purple());
 		test.open("index.html");
+		test
+			.execute(function(){
+				this.data("undoHistory", window.terrain.undoHistory);
+				this.data("redoHistory", window.terrain.redoHistory);
+			})
+			.click('#claimed_earth_p1')
+			.click('#col_6_1')
+			.click('#col_6_2')
+			.execute(function(){
+				this.assert.ok(window.terrain.undoHistory.length == this.data("undoHistory").length + 2, "undoHistory.length should be " + (this.data("undoHistory").length + 2) + ", was " + window.terrain.undoHistory.length);
+				this.assert.ok(window.terrain.redoHistory.length == this.data("redoHistory").length, "redoHistory.length should be " + this.data("redoHistory").length + ", was " + window.terrain.redoHistory.length);
+				this.data("mapData", window.terrain.exportData());
+			});
+
+		undo_redo.undo(test, true, makeShot);
+		undo_redo.redo(test, true, makeShot);
+
+		// Import map
+		exchange.importMap(test, true, makeShot);
+		undo_redo.undo(test, true, makeShot);
+		test.execute(function(){
+			this.assert.ok(window.terrain.exportData() == this.data("mapData"), "exported map should be the same as before action");
+		});
+		undo_redo.redo(test, true, makeShot);
+
+		// Import csv
+		exchange.importCSVMap(test, true, makeShot);
+		undo_redo.undo(test, true, makeShot);
+		test.execute(function(){
+			this.assert.ok(window.terrain.exportData() == this.data("mapData"), "exported map should be the same as before action");
+		});
+		undo_redo.redo(test, true, makeShot);
+
+		test.execute(function(){
+			this.data("undoHistory", window.terrain.undoHistory);
+			this.data("redoHistory", window.terrain.redoHistory);
+		});
+
+		test.done();
+	},
+	"Undomanager part 2": function(test) {
+		/*test
+			.click('#claimed_earth_p1')
+			.click('#col_6_1')
+			.click('#col_6_2')
+			.execute(function(){
+				this.data("undoHistory", window.terrain.undoHistory);
+				this.data("redoHistory", window.terrain.redoHistory);
+				this.data("mapData", window.terrain.exportData());
+			});
+			// Resize map
+		resize_map.top_add(test, true, makeShot);
+		resize_map.left_add(test, true, makeShot);
+		resize_map.right_add(test, true, makeShot);
+		resize_map.bottom_add(test, true, makeShot);
+		test
+			.execute(function(){
+				this.assert.ok(window.terrain.undoHistory.length == this.data("undoHistory").length + 4, "undoHistory.length should be " + (this.data("undoHistory").length  + 4) + ", was " + window.terrain.undoHistory.length);
+				this.assert.ok(window.terrain.redoHistory.length == this.data("redoHistory").length, "redoHistory.length should be " + this.data("redoHistory").length + ", was " + window.terrain.redoHistory.length);
+			})
+			.click("#undo")
+			.click("#undo")
+			.click("#undo")
+			.click("#undo")
+			.execute(function(){
+				this.assert.ok(window.terrain.exportData() == this.data("mapData"), "exported map should be the same as before action");
+			});
 		/*
-			setting a tile
-			import map
-			import csv
 			mirror
-			resize map
 		*/
 		test.done();
 	}
